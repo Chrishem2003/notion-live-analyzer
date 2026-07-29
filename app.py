3348fd75442806ca506b551e7a0815e55bad3f25
Chrishem Autonomous Engine <engine@chrishem.internal>
2026-07-29 11:07:30 +0300

    Integrate Admin Privileges & Billing module into master app.py navigation: 2026-07-29 11:07:29

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from modules.orbital_relay import render_orbital_relay_panel
 from modules.biodefense_core import render_biodefense_panel
 from modules.sovereign_singularity import render_sovereign_singularity_panel
 from modules.ai_intelligence_daemon import render_ai_intelligence_panel
+from modules.admin_billing_core import render_admin_billing_panel
 
 # Initialize Database Backend
 init_db()
@@ -137,5 +138,6 @@ elif navigation == "Biodefense & Pathogen Surveillance":
     render_biodefense_panel()
 elif navigation == "Sovereign Singularity Core":
     render_sovereign_singularity_panel()
+
 
 
