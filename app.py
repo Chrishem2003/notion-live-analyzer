c1c7ff3eadc9e6a618c85c67b0afe50fca67c818
Chrishem Autonomous Engine <engine@chrishem.internal>
2026-07-29 11:02:46 +0300

    Integrate Quantum Core & Neural Defense module into master app.py navigation: 2026-07-29 11:02:46

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ from modules.security_auditor import render_security_audit_panel
 from modules.backup_engine import render_backup_panel
 from modules.performance_profiler import render_profiler_panel
 from modules.threat_response import render_threat_response_panel
+from modules.quantum_core import render_quantum_core_panel
 from modules.aes_vault import generate_aes_key
 from modules.email_reports import send_audit_email
 from modules.stripe_verification import render_subscription_panel
@@ -175,6 +176,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
