369598c5970265d474094865de2d1dc0135ff63c
Chrishem Autonomous Engine <engine@chrishem.internal>
2026-07-29 11:08:47 +0300

    Integrate Telemetry & Smart Alerts module into master app.py navigation: 2026-07-29 11:08:47

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from modules.sovereign_singularity import render_sovereign_singularity_panel
 from modules.ai_intelligence_daemon import render_ai_intelligence_panel
 from modules.admin_billing_core import render_admin_billing_panel
 from modules.personal_workspace import render_personal_workspace_panel
+from modules.telemetry_alerting import render_telemetry_alerting_panel
 
 # Initialize Database Backend
 init_db()
@@ -139,6 +140,7 @@ elif navigation == "Biodefense & Pathogen Surveillance":
     render_biodefense_panel()
 elif navigation == "Sovereign Singularity Core":
     render_sovereign_singularity_panel()
+
 
 
 
