b0e1187726b883e9368c45bf129cdcda36748f80
Chrishem Autonomous Engine <engine@chrishem.internal>
2026-07-31 12:25:20 +0300

    Restore Secure Personal Vault page in pages directory

---
+++ pages/44_🔐_Secure_Personal_Vault.py
@@ -0,0 +1,11 @@
+﻿import streamlit as st
+from modules.secure_personal_vault import *
+
+# Execute/render the module UI
+if __name__ == "__main__":
+    if "main" in dir():
+        main()
+    elif "render" in dir():
+        render()
+    elif "show" in dir():
+        show()
